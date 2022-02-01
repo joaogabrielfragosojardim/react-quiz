664d6f58aaf2eb56693ea0c122a4fb205bafa774
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2022-02-01 09:33:04 -0300

    resolving type

--- src/Context/QuestionNumber.tsx
+++ src/Context/QuestionNumber.tsx
@@ -1,18 +1,17 @@
-import React, {
-  createContext,
-  useState,
-  useContext,
-  Dispatch,
-} from "react";
+import React, { createContext, useState, useContext, Dispatch } from "react";
 
 interface States {
   questionNumber: number;
   setQuestionNumber: Dispatch<React.SetStateAction<number>>;
 }
 
+interface ContextProps {
+  children: React.ReactNode;
+}
+
 const QuestionNumberContext = createContext({} as States);
 
-export function QuestionNumberProvider({ children }: any) {
+export function QuestionNumberProvider({ children }: ContextProps) {
   const [questionNumber, setQuestionNumber] = useState(0);
   return (
     <QuestionNumberContext.Provider
